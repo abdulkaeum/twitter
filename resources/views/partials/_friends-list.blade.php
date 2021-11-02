f484aadb21ed7dad3063a5ea4dea43da46fe350c
abdulkaeum <abdul.kaeum@hotmail.com>
2021-11-02 21:25:20 +0000

    allow users to follow one another

--- resources/views/partials/_friends-list.blade.php
+++ resources/views/partials/_friends-list.blade.php
@@ -1,15 +1,15 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach (range(1, 8) as $index)
+    @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
                 <img
                     class="rounded-full mr-3"
-                    src="https://i.pravatar.cc/40"
+                    src="{{ $user->avatar }}"
                     alt=""
                 >
-                John Doe
+                {{ $user->name }}
             </div>
         </li>
     @endforeach
