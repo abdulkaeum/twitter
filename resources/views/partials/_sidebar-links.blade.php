ac855c96c2223245ec5af207672e0bdf4c73d99c
abdulkaeum <abdul.kaeum@hotmail.com>
2021-11-06 15:33:14 +0000

    tidy up menu

--- resources/views/partials/_sidebar-links.blade.php
+++ resources/views/partials/_sidebar-links.blade.php
@@ -1,22 +1,27 @@
 <ul>
     <li>
         <a class="font-bold text-lg mb-4 block" href="/">
-            Home
+            <i class="fas fa-home mr-2"></i> Home
         </a>
     </li>
     <li>
         <a class="font-bold text-lg mb-4 block" href="{{ route('profile', auth()->user()) }}">
-            Profile
+            <i class="fas fa-user mr-2"></i> Profile
         </a>
     </li>
     <li>
         <a class="font-bold text-lg mb-4 block" href="{{ route('explore.index') }}">
-            Explore
+            <i class="fas fa-hashtag mr-2"></i> Explore
         </a>
     </li>
     <li>
         <a class="font-bold text-lg mb-4 block" href="/">
-            Home
+            <i class="far fa-bookmark mr-2"></i> Bookmarks
+        </a>
+    </li>
+    <li>
+        <a class="font-bold text-lg mb-4 block" href="/">
+            <i class="far fa-envelope mr-1"></i> Messages
         </a>
     </li>
 </ul>
